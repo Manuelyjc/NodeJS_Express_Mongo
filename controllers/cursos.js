3aba75a29fd4c3a62c8699fdf31a98e8b78e5b9c
Camilo13056 <camilo13056@gmail.com>
2024-03-06 17:24:21 -0500

    Recuperacion de archivos

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -1,12 +1,50 @@
 const express = require('express');
-const logic = require('../models/curso_model');
+const logic = require('../logic/curso_logic');
 const ruta = express.Router();
 
-
+// Endpoint de tipo GET para el recurso Cursos
 ruta.get('/',(req, res) => {
-    res.json('respuesta por peticion get de cursos funcionando correctamente...')
+    let resultado = logic.listarCursosActivos();
+    resultado.then(cursos => {
+        res.json(cursos);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
 });
 
+// Endpoint de tipo POST para el recurso CURSOS
+ruta.post('/', (req, res) => {
+    let resultado = logic.crearCurso(req.body);
 
+    resultado.then(curso => {
+        res.json({
+            curso
+        })
+    }).catch(err => {
+        res.status(400).json({
+            err
+        })
+    })
+});
+
+// Endpoint de tipo PUT para el recurso CURSOS
+ruta.put('/:id', (req, res) => {
+    let resultado = logic.actualizarCurso(req.params.id, req.body);
+    resultado.then(curso => {
+        res.json(curso)
+    }).catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+// Endpoint de tipo DELETE para el recurso CURSOS
+ruta.delete('/:id', (req, res) => {
+    let resultado = logic.desactivarCurso(req.params.id);
+    resultado.then(curso => {
+        res.json(curso);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
 
 module.exports = ruta;
